530c058ac69d02bf730e56d5d199429e5ead3bf5
Vivek Rai <vivekrai.iitkgp@gmail.com>
2017-07-17 17:11:53 +0530

    Update
    Signed-off-by: Vivek Rai <vivekrai.iitkgp@gmail.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+== Dotfiles ==
+
+Current configuration on my Ubuntu machine with i3, Zsh, and Vim. Use GNU stow
+to setup the configuration.
+
+== Installation ==
+
+```
+git clone https://github.com/vivekiitkgp/dotfiles
+cd dotfiles
+```
+
+Stow directories as needed.
+
+```
+stow vim
+stow i3
+stow utils
+```
---
+++ fzf/.fzf.bash
@@ -0,0 +1,20 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/vivekrai/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/vivekrai/.fzf/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" == */home/vivekrai/.fzf/man* && -d "/home/vivekrai/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:/home/vivekrai/.fzf/man"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/vivekrai/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/vivekrai/.fzf/shell/key-bindings.bash"
+
